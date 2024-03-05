c5a1743e9abc5d9d72a2f77c558df2892e52219b
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-03-05 16:12:59 -0300

    Quicksort v1 corrigido

--- matriz-quicksort.c
+++ matriz-quicksort.c
@@ -34,27 +34,35 @@ void matrizbase(int quantidade) {
 }
 
 void quicksort(int matrizVerificar[], int linhas) {
-
-    int posicao;
-    int direita = linha - 1;
-    int esquerda = 0;
-    int veri = matrizVerificar[0];
     
-    while(esquerda <= direita) {
-        
-        while(matrizVerificar[esquerda] < veri) esquerda++;
-        while(matrizVerificar[direita] >= veri) direita++;
-        
-        if(esquerda <= direita) {
-            (matrizVerificar[esquerda], matrizVerificar[direita]) = (matrizVerificar[direita], matrizVerificar[esquerda]);
+    int posicao;
+    int direita = linhas - 1;
+    int esquerda = 0;
+    int referncia = matrizVerificar[0];
+    
+
+    while (esquerda <= direita) {
+        while (matrizVerificar[esquerda] < referncia) {
+            esquerda++;
+        }
+        while (matrizVerificar[direita] >= referncia) {
+            direita--;
+        }
+
+        if (esquerda <= direita) {
+            int veri = matrizVerificar[esquerda]; 
+            matrizVerificar[esquerda] = matrizVerificar[direita];
+            matrizVerificar[direita] = veri; // swanp onde e necessario mais um elemento para inverter a ordem
             esquerda++;
             direita--;
+            
+            //nao precisa de parentes so da matrizVerificar[]
         }
     }
 
-    printf("\n\nMatriz ordenada.");
-    
+    printf("\n\nMatriz ordenada:");
+
     for (posicao = 0; posicao < linhas; posicao++) {
-        printf(" [%d]", matrizVerificar[i]);
+        printf(" [%d]", matrizVerificar[posicao]);
     }
 }
