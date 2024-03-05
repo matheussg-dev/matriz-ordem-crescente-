ad91460867ceaaec01a14d473671a2936e2862da
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-03-05 13:05:14 -0300

    teste

--- matriz.c
+++ matriz.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
 
-#define linhaQuantidade 6
-
-char matrizCrecente[linhaQuantidade];
+char matrizCrecente[int quantidade];
 
 int matrizbase();
 void bublesort();
 
 int main() {
-
-    matrizbase();
-    bublesort();
-
+    
+    int tamanho;
+    
+    printf("Defina o tamanho da matriz.\n");
+    scanf("%i", &tamanho);
+    
+    matrizbase(tamanho);
+    
 }
 
-int matrizbase() {
-
+int matrizbase(int quantidade) {
+    
     int linha;
-    int numeros;
-
-    for (linha = 0; linha < linhaQuantidade; linha++) {
+    char matrizCrecente[int quantidade];
+    
+    printf("Tamanho da matriz.\n");
+    scanf("%i", &quantidade);
+    
+    
+    for (linha = 0; linha < quantidade; linha++) {
         matrizCrecente[linha];
 
+        printf("\nnumeros {%d}\n", linha);
+        scanf("%i", &matrizCrecente[linha]);
     }
 }
 
 void bublesort() {
-
+    
 }
