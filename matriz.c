0f59a611cbcb691e1be69960b1abf7715437d9d2
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-03-05 13:04:17 -0300

    base

---
+++ matriz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define linhaQuantidade 6
+
+char matrizCrecente[linhaQuantidade];
+
+int matrizbase();
+void bublesort();
+
+int main() {
+
+    matrizbase();
+    bublesort();
+
+}
+
+int matrizbase() {
+
+    int linha;
+    int numeros;
+
+    for (linha = 0; linha < linhaQuantidade; linha++) {
+        matrizCrecente[linha];
+
+    }
+}
+
+void bublesort() {
+
+}
